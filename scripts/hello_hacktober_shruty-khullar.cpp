46620d7aef37cde17e8623368a137f0bc9353e63
Shruty-Khullar <69571542+Shruty-Khullar@users.noreply.github.com>
2020-10-11 21:00:06 +0530

    Update hello_hacktober_shruty-khullar.cpp

--- scripts/hello_hacktober_shruty-khullar.cpp
+++ scripts/hello_hacktober_shruty-khullar.cpp
@@ -1,9 +1,11 @@
 // LANGUAGE: C++
 // AUTHOR: Shruty Khullar
 // GITHUB: https://github.com/shruty-khullar
+//hacktoberfest
 #include<iostream>
 using namespace std;
 int main()
 {
     cout<<'Hello Hacktober2020';
+    
 }
